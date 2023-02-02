efcff094564f577e31219e3f76246f422e047220
TantariWD <46665152+KirativeWD@users.noreply.github.com>
2023-02-02 11:07:09 -0500

    Added observers and accessibility functions
    Included observer for strength categories to slide up, and JS functions to change the aria-hidden and expanded for categories once clicked on

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,7 +1,8 @@
 const tags = document.querySelectorAll('.tag');
 const fadeSections = document.querySelectorAll('.scrollFade');
+const slideUp = document.querySelectorAll('.slideUp');
 
-// Removes the tags and hide scroll fade from the page if user JS is enabled
+// Removes activated animation classes if user JS is enabled
 tags.forEach((tag) => {
     tag.classList.remove('tagActive');
 });
@@ -10,24 +11,40 @@ fadeSections.forEach((section) => {
     section.classList.remove('fadeActive');
 });
 
+slideUp.forEach((slide) => {
+    slide.classList.remove('slideActive');
+});
 
 
+// When user scrolls, tag will come into view if in view, hides if out of view
 const tagObserver = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('tagActive');
             return;
-        }
+        };
         entry.target.classList.remove('tagActive');
     });
 });
 
+// When user scrolls, elements fade in and stay in view
 const fadeObserver = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('fadeActive');
-            fadeObserver.disconnect();
-        }
+            fadeObserver.unobserve(entry.target);
+        };
+    });
+});
+
+// When user scrolls, strength categories slide into place
+
+const slideObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('slideActive');
+            fadeObserver.unobserve(entry.target);
+        };
     });
 });
 
@@ -39,3 +56,43 @@ tags.forEach((tag) => {
 fadeSections.forEach((section) => {
     fadeObserver.observe(section);
 });
+
+slideUp.forEach((slide) => {
+    slideObserver.observe(slide);
+});
+
+const checkBoxes = document.querySelectorAll('.display_cat');
+
+// When a dropdown is clicked or pressed in STRENGTHS checkbox is marked as checked and expanded
+checkBoxes.forEach((box) => {
+    box.addEventListener('click', ariaSwitchOne);
+    box.addEventListener('click', ariaSwitchTwo);
+});
+
+checkBoxes.forEach((box) => {
+    box.addEventListener('keydown', ariaSwitchOne);
+    box.addEventListener('keydown', ariaSwitchTwo);
+});
+
+function ariaSwitchOne() {
+    checkBoxes.forEach((check) => {
+        if(check.checked) {
+            check.setAttribute('aria-checked', 'true');
+            check.setAttribute('aria-expanded', 'true');
+        } else {
+            check.setAttribute('aria-checked', 'false');
+            check.setAttribute('aria-expanded', 'false');
+        }
+    });
+}
+
+function ariaSwitchTwo() {
+    checkBoxes.forEach((check) => {
+        var strInfo = check.parentNode.querySelector('.str__info');
+        if(check.checked) {
+            strInfo.setAttribute('aria-hidden', 'false');
+        } else {
+            strInfo.setAttribute('aria-hidden', 'true');
+        }
+    })
+}
