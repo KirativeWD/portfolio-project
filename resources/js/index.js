1aee53571f4666768c74740de9a3dd00df890fc1
TantariWD <46665152+KirativeWD@users.noreply.github.com>
2023-02-25 19:55:36 -0500

    Site Redisgn
    Redid HTML to include on a single page, adjusted CSS and JavaScript for change

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,37 @@
+// Navigation toggling
+
+const navToggle = document.querySelector('.nav_toggle');
+const navItems = document.querySelectorAll('.nav-item');
+const primaryNav = document.querySelector('.primary_nav');
+const body = document.body;
+
+navToggle.addEventListener('click', () => {
+    const isOpen = navToggle.getAttribute('aria-expanded');
+
+    if (isOpen === 'false') {
+        navToggle.setAttribute('aria-expanded', true);
+        primaryNav.setAttribute('data-visible', true);
+        body.classList.toggle('fixed');
+
+    } else {
+        navToggle.setAttribute('aria-expanded', false);
+        primaryNav.setAttribute('data-visible', false);
+        body.classList.toggle('fixed');
+    }
+});
+
+navItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        const isOpen = navToggle.getAttribute('aria-expanded');
+
+        if (isOpen == 'true') {
+            navToggle.setAttribute('aria-expanded', false);
+            primaryNav.setAttribute('data-visible', false);
+            body.classList.toggle('fixed');
+        }
+    });
+});
+
 const tags = document.querySelectorAll('.tag');
 const fadeSections = document.querySelectorAll('.scrollFade');
 const slideUp = document.querySelectorAll('.slideUp');
@@ -61,7 +95,7 @@ slideUp.forEach((slide) => {
     slideObserver.observe(slide);
 });
 
-const checkBoxes = document.querySelectorAll('.display_cat');
+const checkBoxes = document.querySelectorAll('.str_toggle');
 
 // When a dropdown is clicked or pressed in STRENGTHS checkbox is marked as checked and expanded
 checkBoxes.forEach((box) => {
@@ -88,7 +122,7 @@ function ariaSwitchOne() {
 
 function ariaSwitchTwo() {
     checkBoxes.forEach((check) => {
-        var strInfo = check.parentNode.querySelector('.str__info');
+        var strInfo = check.parentNode.querySelector('.str_info');
         if(check.checked) {
             strInfo.setAttribute('aria-hidden', 'false');
         } else {
